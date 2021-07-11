2f2c5a1a794996cdf38d20fac45e26c77881dd10
Alexander Eldeib <alexeldeib@gmail.com>
2021-07-11 02:43:35 -0700

    fix stderr

--- test/e2e/azure_timesync.go
+++ test/e2e/azure_timesync.go
@@ -222,7 +222,7 @@ func AzureDaemonsetTimeSyncSpec(ctx context.Context, inputGetter func() AzureTim
 			// TODO(alexeldeib): possibly mount /proc, or fix the base container so we can just invoke '/nsenter -t 1 -a'
 			cmd := []string{"/nsenter", "-t", "1", "-m", "-u", "-i", "-n", "-p", "-C", "-r", "-w", "--", "bash", "-c", "echo hello && systemctl is-active chronyd && echo chronyd is active"}
 			// command := []string{"systemctl", "is-active", "chronyd", "&&", "echo", "✓ chronyd is active"}
-			stdout, stdout, err := e2e_pod.ExecWithOutput(clientset, config, pod, cmd)
+			stdout, stderr, err := e2e_pod.ExecWithOutput(clientset, config, pod, cmd)
 			if err != nil {
 				Logf("failed to nsenter host %s, error: '%s', stdout:  '%s'", s.Hostname, err, stdout.String())
 				Logf("stdout: '%s'", stdout.String())
