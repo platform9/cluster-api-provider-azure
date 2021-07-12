4bb794efd1f9aacec5233c207dbdd659f2478b77
Alexander Eldeib <alexeldeib@gmail.com>
2021-07-11 18:11:57 -0700

    only nsenter linux nodes

--- exp/controllers/azuremanagedmachinepool_reconciler.go
+++ exp/controllers/azuremanagedmachinepool_reconciler.go
@@ -142,7 +142,8 @@ func (s *azureManagedMachinePoolService) Reconcile(ctx context.Context, scope *s
 	}
 
 	matchingLabels := client.MatchingLabels(map[string]string{
-		"agentpool": scope.InfraMachinePool.Name,
+		"agentpool":        scope.InfraMachinePool.Name,
+		"kubernetes.io/os": "linux", // windows nodes don't support daemonsets (still?) and definitely don't support nsenter
 	})
 
 	scope.V(2).Info(fmt.Sprintf("Listing node in agentpool '%s'", scope.InfraMachinePool.Name))
